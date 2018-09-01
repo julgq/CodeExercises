3bd3e97cdd679d7d81bbb82636c42d96b769b177
Julio Gutierrez <julgq@iMac-de-Julio.local>
2018-09-01 15:39:35 -0500

    chunk problem

--- chunk/index.js
+++ chunk/index.js
@@ -8,6 +8,53 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    const chunked = [];
+
+    for (let element of array) {
+        const last = chunked[chunked.length - 1];
+        console.log(last);
+
+        if (!last || last.length === size) {
+            chunked.push([element]);
+        } else {
+            last.push(element);
+        }
+
+    }
+
+    return chunked;
+}
+/* 
+// - Solution JCCGQ 
+function chunk(array, size) {
+
+    let arr = [];
+    let temp = [];
+    let acum = 0;
+
+    for (let i = 0; i <= array.length; i++) {
+        acum++;
+        if (acum <= size) {
+
+            if (array[i] !== undefined) {
+                temp.push(array[i]);
+
+            }
+
+
+        }
+
+        if (acum === size || i === array.length) {
+            acum = 0;
+            arr.push(temp);
+            temp = [];
+        }
+    }
+
+    return arr;
+
+}
+*/
 
 module.exports = chunk;

e7e9652991e293320e4f1fee3428c6fe9ba01e24
Julio <jc.gutierrezq@gmail.com>
2019-08-13 21:12:33 -0500

    steps

--- steps/index.js
+++ steps/index.js
@@ -17,6 +17,42 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+
+  for(let row = 0; row < n; row++){
+    let stair = '';
+    
+    for(let column = 0; column < n; column++) {
+      if(column <= row){
+        stair += '#';
+      } else {
+        stair += ' ';
+      }
+    }
+    console.log(stair);
+  }
+}
+
+/*
+function steps(n) {
+  var matrix = [[], []];
+  var matrixSize = n;
+  var stringLog = [];
+
+  for (var row = 0; row <= matrixSize; row++) {
+    for (var col = 0; col <= matrixSize; col++) {
+      console.log(row, col);
+      if (col <= row) {
+        matrix[row][col] = '#';
+      } else {
+        matrix[row][col] = ' ';
+      }
+    }
+
+  }
+  console.log(matrix);
+}
+*/
+
 
 module.exports = steps;

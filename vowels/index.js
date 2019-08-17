d91df8c8e554dfef3a976031b54a643285ad7447
Julio <jc.gutierrezq@gmail.com>
2019-08-17 18:37:27 -0500

    regular expressions

--- vowels/index.js
+++ vowels/index.js
@@ -26,9 +26,33 @@ function vowels(str) {
     }
 
     return numberOfVoewls;
-
 }
 
 */
+
+// Solution 2 - .includes function
+/*function vowels(str) {
+    let count = 0;
+    const checker = ['a', 'e', 'i', 'o', 'u'];
+
+    for (let char of str.toLowerCase()) {
+        if (checker.includes(char)) {
+            count++;
+        }
+    }
+
+    return count;
+}
+*/
+
+// Solution 3 - Regular Expressions
+
+function vowels(str) {
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+}
+
+
+
 
 module.exports = vowels;
